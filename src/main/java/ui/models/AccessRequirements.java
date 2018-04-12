2cd75c5bb950e68a07c74a2dd7e74bc3ec9f8bc1
Pavlo <pavlo.bendus@gmail.com>
2018-04-12 23:49:34 +0300

    javafx models added

---
+++ src/main/java/ui/models/AccessRequirements.java
@@ -0,0 +1,44 @@
+package ui.models;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class AccessRequirements {
+
+    private IntegerProperty id;
+    private StringProperty name;
+
+    public AccessRequirements() {
+    }
+
+    public AccessRequirements(int id, String name) {
+        this.id = new SimpleIntegerProperty(id);
+        this.name = new SimpleStringProperty(name);
+    }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+}
