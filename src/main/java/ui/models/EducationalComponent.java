f297e3857e855a288eaa8db949fd65087d2f63a8
Andriy Deputat <andriy.deputat@gmail.com>
2018-05-16 13:29:12 +0300

    ui EducationalComponent and Template changed

--- src/main/java/ui/models/EducationalComponent.java
+++ src/main/java/ui/models/EducationalComponent.java
@@ -4,124 +4,97 @@ import javafx.beans.property.*;
 
 public class EducationalComponent {
 
-    private IntegerProperty id;
-    private IntegerProperty credit;
-    private StringProperty courseTitle;
-    private IntegerProperty nationalScore;
-    private ObjectProperty<EducationalComponentType> educationalComponentType;
-    private ObjectProperty<RatingPoint> ratingPoint;
-    private ObjectProperty<NationalGrade> nationalGrade;
-    private ObjectProperty<Diploma> diploma;
+  private IntegerProperty id;
+  private IntegerProperty nationalScore;
+  private ObjectProperty<EducationalComponentTemplate> educationalComponentTemplate;
+  private ObjectProperty<RatingPoint> ratingPoint;
+  private ObjectProperty<NationalGrade> nationalGrade;
+  private ObjectProperty<Diploma> diploma;
 
-    public EducationalComponent() {
-    }
+  public EducationalComponent() {
+  }
 
-    public EducationalComponent(int id, int credit, String courseTitle, int nationalScore,
-                                EducationalComponentType educationalComponentType, RatingPoint ratingPoint,
-                                NationalGrade nationalGrade, Diploma diploma) {
-        this.id = new SimpleIntegerProperty(id);
-        this.credit = new SimpleIntegerProperty(credit);
-        this.courseTitle = new SimpleStringProperty(courseTitle);
-        this.nationalScore = new SimpleIntegerProperty(nationalScore);
-        this.educationalComponentType = new SimpleObjectProperty<>(educationalComponentType);
-        this.ratingPoint = new SimpleObjectProperty<>(ratingPoint);
-        this.nationalGrade = new SimpleObjectProperty<>(nationalGrade);
-        this.diploma = new SimpleObjectProperty<>(diploma);
-    }
+  public EducationalComponent(int id, int nationalScore,
+      EducationalComponentTemplate educationalComponentTemplate, RatingPoint ratingPoint,
+      NationalGrade nationalGrade, Diploma diploma) {
+    this.id = new SimpleIntegerProperty(id);
+    this.nationalScore = new SimpleIntegerProperty(nationalScore);
+    this.educationalComponentTemplate = new SimpleObjectProperty<>(educationalComponentTemplate);
+    this.ratingPoint = new SimpleObjectProperty<>(ratingPoint);
+    this.nationalGrade = new SimpleObjectProperty<>(nationalGrade);
+    this.diploma = new SimpleObjectProperty<>(diploma);
+  }
 
-    public int getId() {
-        return id.get();
-    }
+  public int getId() {
+    return id.get();
+  }
 
-    public IntegerProperty idProperty() {
-        return id;
-    }
+  public IntegerProperty idProperty() {
+    return id;
+  }
 
-    public void setId(int id) {
-        this.id.set(id);
-    }
+  public void setId(int id) {
+    this.id.set(id);
+  }
 
-    public int getCredit() {
-        return credit.get();
-    }
+  public int getNationalScore() {
+    return nationalScore.get();
+  }
 
-    public IntegerProperty creditProperty() {
-        return credit;
-    }
+  public IntegerProperty nationalScoreProperty() {
+    return nationalScore;
+  }
 
-    public void setCredit(int credit) {
-        this.credit.set(credit);
-    }
+  public void setNationalScore(int nationalScore) {
+    this.nationalScore.set(nationalScore);
+  }
 
-    public String getCourseTitle() {
-        return courseTitle.get();
-    }
+  public EducationalComponentTemplate getEducationalComponentTemplate() {
+    return educationalComponentTemplate.get();
+  }
 
-    public StringProperty courseTitleProperty() {
-        return courseTitle;
-    }
+  public ObjectProperty<EducationalComponentTemplate> educationalComponentTemplateProperty() {
+    return educationalComponentTemplate;
+  }
 
-    public void setCourseTitle(String courseTitle) {
-        this.courseTitle.set(courseTitle);
-    }
+  public void setEducationalComponentTemplate(
+      EducationalComponentTemplate educationalComponentTemplate) {
+    this.educationalComponentTemplate.set(educationalComponentTemplate);
+  }
 
-    public int getNationalScore() {
-        return nationalScore.get();
-    }
+  public RatingPoint getRatingPoint() {
+    return ratingPoint.get();
+  }
 
-    public IntegerProperty nationalScoreProperty() {
-        return nationalScore;
-    }
+  public ObjectProperty<RatingPoint> ratingPointProperty() {
+    return ratingPoint;
+  }
 
-    public void setNationalScore(int nationalScore) {
-        this.nationalScore.set(nationalScore);
-    }
+  public void setRatingPoint(RatingPoint ratingPoint) {
+    this.ratingPoint.set(ratingPoint);
+  }
 
-    public EducationalComponentType getEducationalComponentType() {
-        return educationalComponentType.get();
-    }
+  public NationalGrade getNationalGrade() {
+    return nationalGrade.get();
+  }
 
-    public ObjectProperty<EducationalComponentType> educationalComponentTypeProperty() {
-        return educationalComponentType;
-    }
+  public ObjectProperty<NationalGrade> nationalGradeProperty() {
+    return nationalGrade;
+  }
 
-    public void setEducationalComponentType(EducationalComponentType educationalComponentType) {
-        this.educationalComponentType.set(educationalComponentType);
-    }
+  public void setNationalGrade(NationalGrade nationalGrade) {
+    this.nationalGrade.set(nationalGrade);
+  }
 
-    public RatingPoint getRatingPoint() {
-        return ratingPoint.get();
-    }
+  public Diploma getDiploma() {
+    return diploma.get();
+  }
 
-    public ObjectProperty<RatingPoint> ratingPointProperty() {
-        return ratingPoint;
-    }
+  public ObjectProperty<Diploma> diplomaProperty() {
+    return diploma;
+  }
 
-    public void setRatingPoint(RatingPoint ratingPoint) {
-        this.ratingPoint.set(ratingPoint);
-    }
-
-    public NationalGrade getNationalGrade() {
-        return nationalGrade.get();
-    }
-
-    public ObjectProperty<NationalGrade> nationalGradeProperty() {
-        return nationalGrade;
-    }
-
-    public void setNationalGrade(NationalGrade nationalGrade) {
-        this.nationalGrade.set(nationalGrade);
-    }
-
-    public Diploma getDiploma() {
-        return diploma.get();
-    }
-
-    public ObjectProperty<Diploma> diplomaProperty() {
-        return diploma;
-    }
-
-    public void setDiploma(Diploma diploma) {
-        this.diploma.set(diploma);
-    }
+  public void setDiploma(Diploma diploma) {
+    this.diploma.set(diploma);
+  }
 }
